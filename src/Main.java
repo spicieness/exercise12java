186fb49e43527affd58abe630dca2b22aeae1d55
117957 <dylanolson23@aol.com>
2024-04-09 14:37:15 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+class StackFullException extends Exception {}
+
+class StackEmptyException extends Exception {}
+
+
+class Queue<E> {
+    private E[] elements;
+    private int index = 0;
+    private int size;
+
+    Queue(int size) {
+        elements = (E[]) new Object[size];
+        this.size = size;
+    }
+
+    void push(E element ) throws StackFullException {
+        if (index >= size) {
+            throw new StackFullException();
+        }
+
+        elements[index] = element;
+        index++;
+    }
+
+    E pop () throws StackEmptyException {
+        if (index == 0) {
+            throw new StackEmptyException();
+        }
+
+        E returnElement = elements[index - 2];
+        index++;
+        return returnElement;
+    }
+
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Queue<String> strings = new Queue<>(2);
+        try {
+            strings.push("Hello");
+            strings.push("World");
+            System.out.println(strings.pop());
+            System.out.println(strings.pop());
+        } catch (StackFullException | StackEmptyException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
